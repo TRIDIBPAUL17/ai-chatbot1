646a9c2b767ae7a7826f1b5f29b00551fed848da
TRIDIBPAUL17 <137639265+TRIDIBPAUL17@users.noreply.github.com>
2024-08-10 19:19:14 +0530

    Add files via upload

---
+++ chunks/postcss_config_js_transform_ts_23d291._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "chunks/postcss_config_js_transform_ts_23d291._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "chunks/postcss_config_js_transform_ts_23d291._.js",
+  "chunks": [
+    "chunks/node_modules_b3d708._.js",
+    "chunks/_0b8f1c._.js"
+  ],
+  "source": "dynamic"
+});
